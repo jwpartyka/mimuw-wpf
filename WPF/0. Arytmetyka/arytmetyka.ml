feb796b134fa4526ffc13dabe7dec84181683724
Janusz Partyka <janusz.partyka00@gmail.com>
2019-10-20 02:37:34 +0200

    Minor fixes

--- WPF/0. Arytmetyka/arytmetyka.ml
+++ WPF/0. Arytmetyka/arytmetyka.ml
@@ -1,8 +1,9 @@
+(* Typ reprezentujący jednoczęściowy zbiór liczb. *)
 type interval = {l : float; r : float}
 type wartosc =
-    | Pusta
-    | Jeden of interval
-    | Dwa of interval * interval
+    | Pusty                         (* Zbiór przedział.      *)
+    | Jeden of interval             (* Zbiór jednoczęściowy. *)
+    | Dwa of interval * interval    (* Zbiór dwuczęściowy.   *)
 
 let wartosc_dokladnosc x p =
     let procent = abs_float ((p /. 100.) *. x) in
@@ -20,41 +21,47 @@ let in_wartosc x y =
     match x with
     | Jeden a -> a.l <= y && y <= a.r
     | Dwa (a, b) -> (a.l <= y && y <= a.r) || (b.l <= y && y <= b.r)
-    | Pusta -> false
+    | Pusty -> false
 
 let min_wartosc x =
     match x with
     | Jeden a -> a.l
     | Dwa (a, _) -> a.l
-    | Pusta -> nan
+    | Pusty -> nan
 
 let max_wartosc x =
     match x with
     | Jeden a -> a.r
     | Dwa (_, b) -> b.r
-    | Pusta -> nan
+    | Pusty -> nan
 
 let sr_wartosc x =
-    if x = Pusta then nan else
+    if x = Pusty then nan else
     let (mini, maks) = (min_wartosc x, max_wartosc x) in
     if mini = neg_infinity && maks = infinity then nan
     else (mini +. maks) /. 2.0
 
-(* Zwraca sume zbiorow x i y *)
+(* Procedura, która zwraca sumę zbiorów wartości niedokładnych x i y. *)
 let rec union x y =
-    match x, y with
-    | Jeden a, Jeden b -> if b.l < a.l then union y x
+    (* Pomocnicza procedura, która sumuje dwa jednoczęściowe zbiory. *)
+    let rec pom a b =
+        if b.l < a.l then pom b a
         else if a.l <= b.l && b.r <= a.r then Jeden a
         else if b.l <= a.r then Jeden {a with r = b.r}
-        else Dwa (a, b)
+        else Dwa (a, b) in
+
+    match x, y with
+    | Jeden a, Jeden b -> pom a b
     | Jeden a, Dwa (c, d) -> union (union x (Jeden c)) (Jeden d)
     | Dwa (a, b), Jeden c -> union (Jeden a) (union (Jeden b) (Jeden c))
-    | Dwa (a, b), Dwa (c, d) -> union (union (Jeden a) (Jeden c))
-        (union (Jeden b) (Jeden d))
-    | Pusta, _ -> y
-    | _, Pusta -> x
+    | Dwa (a, b), Dwa (c, d) ->
+        union (union (Jeden a) (Jeden c)) (union (Jeden b) (Jeden d))
+    | Pusty, _ -> y
+    | _, Pusty -> x
 
 let rec plus x y =
+    (* Pomocnicza procedura, która wykonuje operację plus a b *)
+    (* na dwóch jednoczęściowych zbiorach a i b.              *)
     let pom a b =
         if a.r +. b.r = 0. then Jeden {l = a.l +. b.l; r = -0.}
         else Jeden {l = a.l +. b.l; r = a.r +. b.r} in
@@ -63,9 +70,11 @@ let rec plus x y =
     | Jeden a, Jeden c -> pom a c
     | Jeden a, Dwa (c, d) -> union (plus x (Jeden c)) (plus x (Jeden d))
     | Dwa (a, b), _ -> union (plus (Jeden a) y) (plus (Jeden b) y)
-    | _, _ -> Pusta
+    | _, _ -> Pusty
 
 let rec minus x y =
+    (* Pomocnicza procedura, która wykonuje operację minus a b *)
+    (* na dwóch jednoczęściowych zbiorach a i b.               *)
     let pom a b =
         if a.r -. b.l = 0. then Jeden {l = a.l -. b.r; r = -0.}
         else Jeden {l = a.l -. b.r; r = a.r -. b.l} in
@@ -74,9 +83,11 @@ let rec minus x y =
     | Jeden a, Jeden c -> pom a c
     | Jeden _, Dwa (c, d) -> union (minus x (Jeden c)) (minus x (Jeden d))
     | Dwa (a, b), _-> union (minus (Jeden a) y) (minus (Jeden b) y)
-    | _, _ -> Pusta
+    | _, _ -> Pusty
 
 let rec razy x y =
+    (* Pomocnicza procedura, która wykonuje operację razy a b *)
+    (* na dwóch jednoczęściowych zbiorach a i b.              *)
     let rec pom a b =
         if a = {l = 0.; r = 0.} || b = {l = 0.; r = 0.} then
             Jeden {l = 0.; r = 0.}
@@ -95,11 +106,13 @@ let rec razy x y =
     | Jeden a, Jeden c -> pom a c
     | Jeden _, Dwa (c, d) -> union (razy x (Jeden c)) (razy x (Jeden d))
     | Dwa (a, b), _ -> union (razy (Jeden a) y) (razy (Jeden b) y)
-    | _, _ -> Pusta
+    | _, _ -> Pusty
 
 let rec podzielic x y =
+    (* Pomocnicza procedura, która wykonuje operację podzielic a b *)
+    (* na dwóch jednoczęściowych zbiorach a i b .                  *)
     let rec pom a b =
-        if b = {l = 0.; r = 0.} then Pusta
+        if b = {l = 0.; r = 0.} then Pusty
         else if a = {l = 0.; r = 0.} then Jeden {l = 0.; r = 0.}
         else if a.l *. a.r < 0. then
             union (pom {a with r = -0.} b) (pom {a with l = 0.} b)
@@ -114,6 +127,7 @@ let rec podzielic x y =
 
     match x, y with
     | Jeden a, Jeden c -> pom a c
-    | Jeden _, Dwa (c, d) -> union (podzielic x (Jeden c)) (podzielic x (Jeden d))
+    | Jeden _, Dwa (c, d) ->
+        union (podzielic x (Jeden c)) (podzielic x (Jeden d))
     | Dwa (a, b), _ -> union (podzielic (Jeden a) y) (podzielic (Jeden b) y)
-    | _, _ -> Pusta
+    | _, _ -> Pusty
