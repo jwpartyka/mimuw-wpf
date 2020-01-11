d970f5f72d233ec2de7b39716e50440020020e0d
Janusz Partyka <janusz.partyka00@gmail.com>
2020-01-11 19:11:40 +0100

    Final version

--- WPF/4. Topol/topol.ml
+++ WPF/4. Topol/topol.ml
@@ -4,21 +4,21 @@
 
 open PMap
 
-(* Wyjątek podnoszony przez topol, gdy zależności są cykliczne *)
+(* Wyjątek podnoszony przez topol, gdy zależności są cykliczne. *)
 exception Cykliczne
 
 (* Dla danej listy [(a_1,[a_11;...;a_1n]); ...; (a_m,[a_m1;...;a_mk])] *)
 (* zwraca listę, na której każdy z elementów a_i oraz a_ij występuje   *)
 (* dokładnie raz i która jest uporządkowana w taki sposób, że każdy    *)
-(* element a_i jest przed każdym z elementów a_i1 ... a_il             *)
-let topol lista =
+(* element a_i jest przed każdym z elementów a_i1 ... a_il.            *)
+let topol edges =
   (* Procedura, która do listy sąsiadów wierzchołka v w mapie m            *)
   (* dodaje wszystkie wierzchołki z listy l i zwraca zaktuazlizowaną mapę. *)
-  let add_edges m (v, l) =
-    if exists v m then
-      let (vis, adj) = find v m in
-      add v (vis, l @ adj) m
-    else add v (0, l) m in
+  let add_edges g (v, l) =
+    if exists v g then
+      let (vis, adj) = find v g in
+      add v (vis, l @ adj) g
+    else add v (0, l) g in
 
   (* Procedura przetwarzająca wierzchołek v w DFS i wywołująca się na       *)
   (* wszystkich jego sąsiadach. Po przetworzeniu, wierzchołek jest dodawany *)
@@ -32,8 +32,8 @@ let topol lista =
     else if vis = 1 then raise Cykliczne
     else (topo, graph) in
 
-  let wyj = List.map fst lista
-  and wej = List.flatten (List.map snd lista) in
-  let graph = List.fold_left (fun a x -> add x (0, []) a) empty wej in
-  let graph = List.fold_left add_edges graph lista in
-  List.fold_left dfs ([], graph) wyj |> fst
+  let tails = List.map fst edges
+  and heads = List.flatten (List.map snd edges) in
+  let graph = List.fold_left (fun g v -> add v (0, []) g) empty heads in
+  let graph = List.fold_left add_edges graph edges in
+  List.fold_left dfs ([], graph) tails |> fst
